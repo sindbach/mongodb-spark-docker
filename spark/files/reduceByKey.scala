edd087ce4ac8283487939a3fec0e5703978a5418
Wan Bachtiar <sindbach@gmail.com>
2016-07-12 13:16:57 +1000

    Update the example reduceByKey.scala to use the new spark scala connector

--- spark/files/reduceByKey.scala
+++ spark/files/reduceByKey.scala
@@ -1,14 +1,15 @@
-import org.apache.hadoop.conf.Configuration
 
-import com.mongodb.hadoop.io.BSONWritable;
+import com.mongodb.spark._
+import org.bson.Document 
 
-import com.mongodb.hadoop.MongoInputFormat;
-
-import org.bson.BSONObject
-import org.bson.BasicBSONObject
-import org.joda.time.DateTime
+/* Load collection as RDD */
+val rdd = MongoSpark.load(sc)
+println("Input Count: " + rdd.count)
+println("Input documents: ")
+rdd.foreach(println)
 
 /*
+ PROCESSING  
  For example, if you have 4 documents of :
 
 { "doc": "A", "timestamp" : ISODate("2016-02-15T00:43:04.686Z"), "myid" : 1 }
@@ -16,35 +17,43 @@ import org.joda.time.DateTime
 { "doc": "C", "timestamp" : ISODate("2016-01-03T00:43:07.534Z"), "myid" : 1 }
 { "doc": "D", "timestamp" : ISODate("2016-01-03T00:43:09.214Z"), "myid" : 2 }
 
-Grouping by `myid` sort latest timestamp, would return only two documents, doc:A and doc:B. 
+Group by `myid` sort latest timestamp, would return only two documents, doc:A and doc:B. 
 Removing duplicates of myid’s by returning only documents with the latest timestamp.
 */
-
-val mongoConfig = new Configuration()
-mongoConfig.set("mongo.input.uri", "mongodb://mongodb:27017/spark.times")
-mongoConfig.set("mongo.input.query", "{'myid':{$in:[1,2,3,4,5]}}")
-mongoConfig.set("mongo.input.sort", "{timestamp:-1}")
-
-val documents = sc.newAPIHadoopRDD(mongoConfig, classOf[MongoInputFormat], classOf[Object], classOf[BSONObject])
-// print out inputs
-documents.foreach(println)
-
-val outputRDD = documents.map(
-            (tuple)=>((tuple._2.get("myid")), (tuple._2.get("timestamp")))
+import org.joda.time.DateTime
+val outputRDD = rdd.map(
+            (tuple)=>((tuple.get("myid")), (tuple.get("timestamp")))
              ).reduceByKey((x, y) => if (new DateTime(x).isAfter(new DateTime(y))) x else y ).map(
-            (tuple)=>{val bson = new BasicBSONObject(); 
-                  bson.put("myid", tuple._1); 
-                  bson.put("timestamp", tuple._2); 
-                  (null, bson)
+            (tuple)=>{val newdoc = new Document(); 
+                  newdoc.append("myid", tuple._1); 
+                  newdoc.append("timestamp", tuple._2); 
+                  newdoc
                   }
         )
-
-// print out output
+println("Spark RDD processing result: ")
 outputRDD.foreach(println)
 
-// save result to MongoDB
-val outputConfig = new Configuration()
-outputConfig.set("mongo.output.uri", "mongodb://mongodb:27017/spark.output")
-outputRDD.saveAsNewAPIHadoopFile("file:///x", classOf[Any], classOf[Any], classOf[com.mongodb.hadoop.MongoOutputFormat[Any, Any]], outputConfig)
 
+/* Similar aggregation as above, but utilising MongoDB aggregation pipeline */
+val aggRdd = rdd.withPipeline(Seq(
+                Document.parse("{$sort:{timestamp:1}}"), 
+                Document.parse("{$group:{_id:{'myid':'$myid'}, record:{'$first':'$$ROOT'}}}"), 
+                Document.parse("{$project:{'_id':0, 'doc':'$record.doc', 'timestamp':'$record.timestamp', 'myid':'$record.myid'}}")
+                )   
+            )
+println("MongoDB aggregation pipeline reult: ")
+aggRdd.foreach(println)
+
+
+// Save result to MongoDB
+// 1) Default 
+import com.mongodb.spark.config._
+aggRdd.saveToMongoDB()
+// 2) Using helper and WriteConfig to modify destination 
+outputRDD.saveToMongoDB(WriteConfig(Map("uri"->"mongodb://mongodb:27017/spark.processing")))
+println("RDD is written to MongoDB")
+
+println("Done")
 System.exit(0);
+
+
